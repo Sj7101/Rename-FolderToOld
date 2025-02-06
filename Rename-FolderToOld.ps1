c57c44977d5918fdb3bb8bc47ca8c59009267d3b
Sj7101 <32309663+Sj7101@users.noreply.github.com>
2025-02-06 11:15:58 -0600

    Create Rename-FolderToOld.ps1

---
+++ Rename-FolderToOld.ps1
@@ -0,0 +1,25 @@
+﻿function Rename-FolderToOld {
+    param (
+        [Parameter(Mandatory=$true)]
+        [array]$folders
+    )
+    
+    foreach ($folder in $folders) {
+        # Check if the folder exists
+        if (Test-Path -Path $folder) {
+            # Get the folder name and the parent directory
+            $parentDirectory = [System.IO.Path]::GetDirectoryName($folder)
+            $folderName = [System.IO.Path]::GetFileName($folder)
+            
+            # Create the new folder name with '_old' suffix
+            $newFolderName = "$folderName" + "_old"
+            $newFolderPath = Join-Path -Path $parentDirectory -ChildPath $newFolderName
+            
+            # Rename the folder by renaming the folder itself
+            Rename-Item -Path $folder -NewName $newFolderName
+            Write-Host "Renamed '$folder' to '$newFolderPath'"
+        } else {
+            Write-Host "Folder '$folder' does not exist."
+        }
+    }
+}
